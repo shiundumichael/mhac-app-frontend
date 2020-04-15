b237c800777a1fe6d9ed2d7dfaae035ab2144dd5
maxwell fundi <maxwellfundi@live.com>
2020-04-15 12:21:15 +0300

    Adding Bella Mama lyrics.

--- src/app/lyrics/songs.ts
+++ src/app/lyrics/songs.ts
@@ -450,12 +450,100 @@ export const Baba_La_Gumbala = {
     ]
 
     }
-
-
     export const Bella_Mama = {
         songTitle: "Bella Mama",
         lyrics: [
-
+            {
+                "lyric": "Bella mama           Bella mama eeeeeeeeeeh ",
+                "startTimeMinute": "0",
+                "startTimeSecond": "0",
+                "endTimeMinute": "0",
+                "endTimeSecond": "2"
+            },
+            {
+                "lyric": "Bella mama            Bella mama eeeeeeeeeeh         ",
+                "startTimeMinute": "0",
+                "startTimeSecond": "2",
+                "endTimeMinute": "0",
+                "endTimeSecond": "6"
+            },
+            {
+                "lyric": "Bella mama           Bella mama eeeeeeeeeeh ",
+                "startTimeMinute": "0",
+                "startTimeSecond": "6",
+                "endTimeMinute": "0",
+                "endTimeSecond": "9"
+            },
+            {
+                "lyric": "Bella mama            Bella mama eeeeeeeeeeh         ",
+                "startTimeMinute": "0",
+                "startTimeSecond": "9",
+                "endTimeMinute": "0",
+                "endTimeSecond": "12"
+            },
+            {
+                "lyric": "Bella mama           Bella mama eeeeeeeeeeh ",
+                "startTimeMinute": "0",
+                "startTimeSecond": "12",
+                "endTimeMinute": "0",
+                "endTimeSecond": "15"
+            },
+            {
+                "lyric": "Bella mama             Bella mama             Bella mama            Bella mama        eeeeeh",
+                "startTimeMinute": "0",
+                "startTimeSecond": "15",
+                "endTimeMinute": "0",
+                "endTimeSecond": "20"
+            },
+            {
+                "lyric": "Bella mama           Bella mama eeeeeeeeeeh",
+                "startTimeMinute": "0",
+                "startTimeSecond": "20",
+                "endTimeMinute": "0",
+                "endTimeSecond": "23"
+            },
+            {
+                "lyric": "Bella mama            Bella mama eeeeeeeeeeh         ",
+                "startTimeMinute": "0",
+                "startTimeSecond": "23",
+                "endTimeMinute": "0",
+                "endTimeSecond": "26"
+            },
+            {
+                "lyric": "Bella mama           Bella mama eeeeeeeeh",
+                "startTimeMinute": "0",
+                "startTimeSecond": "26",
+                "endTimeMinute": "0",
+                "endTimeSecond": "29"
+            },
+            {
+                "lyric": "Bella mama            Bella mama eeeeeeeeeeh         ",
+                "startTimeMinute": "0",
+                "startTimeSecond": "29",
+                "endTimeMinute": "0",
+                "endTimeSecond": "33"
+            },
+            {
+                "lyric": " Bella mama                   Bella mama   eeeeeeh",
+                "startTimeMinute": "0",
+                "startTimeSecond": "33",
+                "endTimeMinute": "0",
+                "endTimeSecond": "37"
+            },
+            {
+                "lyric": "Bella mama             Bella mama             Bella mama            Bella mama        eeeeeh",
+                "startTimeMinute": "0",
+                "startTimeSecond": "37",
+                "endTimeMinute": "0",
+                "endTimeSecond": "44"
+            },
+            {
+                "lyric": " Bella mama          Bella mama eeeeeeeeeh",
+                "startTimeMinute": "0",
+                "startTimeSecond": "44",
+                "endTimeMinute": "0",
+                "endTimeSecond": "49"
+            }
         ]
 
     }
